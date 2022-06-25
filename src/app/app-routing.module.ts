e10098c13b6b16db8906fe0b418fbee0383a3070
afrivera <andresrfelip@hotmail.com>
2022-06-25 18:41:47 -0500

    config in routes useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // add hast in the navigator
+    useHash: false
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
